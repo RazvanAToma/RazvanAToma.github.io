5ca7eddaa0333dc62b8349f8d313e44da5a5873e
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2023-11-04 22:34:12 +0100

    adding password check

---
+++ index.js
@@ -0,0 +1,13 @@
+let correctincorrect = document.getElementById('correctincorrect');
+let password = 'password';
+
+function checkPass() {
+    if (document.getElementById("password").value === password) {
+
+        correctincorrect.innerHTML = ('correct')
+
+    } else {
+        correctincorrect.innerHTML = ('wrong')
+    } 
+}
+    
